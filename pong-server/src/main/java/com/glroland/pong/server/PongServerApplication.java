60862f5545f14f9c4c14771db57e2e8d0607729f
Lee Roland <glroland@hotmail.com>
2021-09-29 18:56:20 -0400

    initial project after spring starter

---
+++ pong-server/src/main/java/com/glroland/pong/server/PongServerApplication.java
@@ -0,0 +1,13 @@
+package com.glroland.pong.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongServerApplication.class, args);
+	}
+
+}
---
+++ pong-server/src/test/java/com/glroland/pong/server/PongServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glroland.pong.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PongServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
