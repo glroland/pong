f469dfea9a8ee524752148e4b6592c18643e701a
Lee Roland <glroland@hotmail.com>
2023-05-02 21:18:36 -0400

    initial revision no logic, just quarkus starter code

---
+++ pong-backend/src/test/java/com/glroland/pong/funqy/cloudevent/FunqyIT.java
@@ -0,0 +1,10 @@
+package com.glroland.pong.funqy.cloudevent;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class FunqyIT extends FunqyTest {
+
+    // Run the same tests
+
+}
